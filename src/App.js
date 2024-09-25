d04a168c0de96a5945faf97cbdd9d358145cde85
Sahu19980000 <newmobilespoint@gmail.com>
2024-09-25 12:00:11 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState ,useEffect} from "react";
+import { getDatabase, ref, push, set,onChildAdded } from "firebase/database";
 
 function App() {
+  const [name, setname] = useState("");
+  const [chats, setChats] = useState([]);
+
+  const [msg, Setmsg] = useState("");
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      setChats(chats=>[...chats,data.val()])
+    });
+  }, [])
+
+  const sendChat = () => {
+   
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name,
+      message: msg,
+    });
+    // const c = [...chats];
+    // c.push({
+    //   name,message:msg
+    // });
+    // setChats(c);
+    Setmsg("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name ? null : (
+        <div>
+          <input
+            type="text"
+            placeholder="enter your name"
+            onInput={(e) => setname(e.target.value)}
+            value={name}
+          ></input>
+        </div>
+      )}
+
+      {name ? (
+        <div>
+          <h1>user :{name}</h1>
+          <div className="chat-container">
+            {chats.map((ele) => {
+              return (
+                <div className={`container ${ele.name === name ? "me" : ""}`}>
+                  <p className="chatbox">
+                    <strong>{ele.name}</strong>
+                    <span>{ele.message}</span>
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
+
+      <div className="btm">
+        <input
+          type="text"
+          placeholder="enter your chat"
+          onChange={(e) => Setmsg(e.target.value)}
+          value={msg}
+        ></input>
+        <button onClick={(e) => sendChat()}>Submit</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8LXIn9jR6EiTHAhd3jtFEiDVXhAa4vds",
+  authDomain: "react-chat-app-4f77b.firebaseapp.com",
+  databaseURL: "https://react-chat-app-4f77b-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-4f77b",
+  storageBucket: "react-chat-app-4f77b.appspot.com",
+  messagingSenderId: "882858358127",
+  appId: "1:882858358127:web:ed7bc75ddb56ef7986249a",
+  measurementId: "G-7JTVKHHFTW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
